4682161083f152a34ad797ea36a0ba445f07252e
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-07-31 02:07:42 +0530

    Stage 4: Full CRUD

--- src/index.ts
+++ src/index.ts
@@ -75,6 +75,50 @@ app.post("/tasks", (req: Request, res: Response) => {
     return res.status(201).json(newTask);
 });
 
+app.put("/tasks/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    const { title, done } = req.body;
+
+    const task = tasks.find((task) => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`,
+        });
+    }
+
+    if (
+        (title !== undefined &&
+            (typeof title !== "string" || title.trim() === "")) ||
+        (done !== undefined && typeof done !== "boolean")
+    ) {
+        return res.status(400).json({
+            error: "Invalid request body",
+        });
+    }
+
+    if (title !== undefined) task.title = title;
+    if (done !== undefined) task.done = done;
+
+    return res.status(200).json(task);
+});
+
+app.delete("/tasks/:id", (req: Request, res: Response) => {
+    const id = Number(req.params.id);
+
+    const index = tasks.findIndex((task) => task.id === id);
+
+    if (index === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`,
+        });
+    }
+
+    tasks.splice(index, 1);
+
+    return res.status(204).send();
+});
+
 app.listen(PORT, () => {
     console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
