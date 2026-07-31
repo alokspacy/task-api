83b9998417f5222b651bf50317a58d0ea1575886
Alok Singh <193059476+alokspacy@users.noreply.github.com>
2026-08-01 01:37:30 +0530

    Stage 3: PostgreSQL repository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,8 +1,8 @@
-import { SQLiteRepository } from "./SQLiteRepository";
+import { PostgresRepository } from "./PostgresRepository";
 import { TaskRepository } from "./TaskRepository";
 
 export * from "./TaskRepository";
 export * from "./SQLiteRepository";
 export * from "./PostgresRepository";
 
-export const taskRepository: TaskRepository = new SQLiteRepository();
+export const taskRepository: TaskRepository = new PostgresRepository();
